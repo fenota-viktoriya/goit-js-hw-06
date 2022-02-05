de2ccef1e8836bc005c38e436339665d3e898956
fenota-viktoriya <Fen9206@gmail.com>
2022-02-05 22:43:42 +0200

    add string

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,12 @@
 const elLi= document.querySelectorAll('.item');   
 console.log(`Number of categories: ${elLi.length}`);
+console.log('');
 
 
 
 [...elLi].map(item => { 
   
-  console.log(`Category: ${item.firstElementChild.textContent}`);
-  
+  console.log(`Category: ${item.firstElementChild.textContent} `);
+  console.log('');
   console.log(`Elements: ${item.lastElementChild.children.length}`);
 });
